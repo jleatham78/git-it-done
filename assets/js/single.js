ef0f377847d381893b6c69fb089061d73a391eac
jleatham78 <janeleleatham01@gmail.com>
2020-12-09 22:14:54 -0700

    linked info from pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,21 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function(repo) {
 
@@ -8,10 +25,15 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
             displayIssues(data);
+
+            //check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
             
         });
     } else {
-        alert("There was a problem with your request");
+        document.location.replace("./index.html");
     }
 
 });
@@ -54,7 +76,21 @@ var displayIssues = function(issues) {
     }
 
 };
+
+var displayWarning = function(repo) {
+    //addd text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+};
+
+
     
 
 
-getRepoIssues("jleatham78/git-it-done");
+getRepoName();
